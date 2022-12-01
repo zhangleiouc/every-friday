691b7214ddec7371d2b6a171bf3eada5025c9a42
zhanglei <zhangleiouc@gmail.com>
2022-12-01 16:55:03 +0800

    函数式2

--- src/main/java/com/example/demo/onjava8/fp/FunctionProgramming.java
+++ src/main/java/com/example/demo/onjava8/fp/FunctionProgramming.java
@@ -3,6 +3,7 @@ package com.example.demo.onjava8.fp;
 public class FunctionProgramming {
 
     public static void main(String[] args){
+        System.out.println("FunctionProgramming");
     }
     
 }
