46ef2d3404edf23e7d6195b3f4cc3c08236efb88
zhanglei <zhangleiouc@gmail.com>
2022-12-10 16:51:15 +0800

    函数式2

--- src/main/java/com/example/demo/onjava8/fp/FunctionProgramming.java
+++ src/main/java/com/example/demo/onjava8/fp/FunctionProgramming.java
@@ -1,8 +1,14 @@
 package com.example.demo.onjava8.fp;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class FunctionProgramming {
 
     public static void main(String[] args){
+        List<String> list = new ArrayList<>();
+        list.stream().map(s -> s + "").collect(Collectors.toList());
         System.out.println("FunctionProgramming");
     }
     
